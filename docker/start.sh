4487243b68eaf59d017d43365fc52168c6550a87
dingxiuan <dingxiuan@163.com>
2020-12-01 23:55:30 +0800

    调通使用Dockerfile构建镜像

--- docker/start.sh
+++ docker/start.sh
@@ -3,6 +3,10 @@
 #source /etc/profile
 cd /opt/app/
 #appname=$(ls|grep .jar | sort -rn)
-appname=$(find /opt/app -type f -name "*.jar" | sort -rn)
+appname=$(find ./ -type f -name "*.jar" | sort -rn)
 name=$(echo $appname|cut -d ' ' -f1)
-java -jar $name -Duser.timezone=GMT+08 -Xmx300m  -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=18001 --spring.config.location=/opt/app/application.properties,/opt/config/conf.properties > /opt/logs/$name.log
+java -jar ./$name \
+  -Duser.timezone=GMT+08 \
+  -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=15001   \
+  --spring.config.location=./application.properties \
+  > /opt/logs/$(echo "$name" | cut -f 1 -d '.').log
