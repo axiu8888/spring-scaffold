7e28ff4e6328f44e44ac5deaf3ca3d5c2fe93b3b
dingxiuan <dingxiuan@163.com>
2020-12-27 12:36:06 +0800

    优化

--- system/src/main/java/com/benefitj/system/config/CommonsConfiguration.java
+++ system/src/main/java/com/benefitj/system/config/CommonsConfiguration.java
@@ -6,9 +6,16 @@ import com.benefitj.scaffold.spring.EnableScaffoldWebSecurityConfiguration;
 import com.benefitj.spring.aop.EnableAutoAopWebHandler;
 import com.benefitj.spring.aop.log.EnableRequestLoggingHandler;
 import com.benefitj.spring.applicationevent.EnableApplicationListener;
+import com.benefitj.spring.security.url.AnnotationUrlRegistryConfigurerCustomizer;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
 import tk.mybatis.spring.annotation.MapperScan;
+
+import java.util.stream.Collectors;
 
 @EntityScan("com.benefitj.system.model")
 @MapperScan("com.benefitj.system.mapper")
@@ -20,4 +27,21 @@ import tk.mybatis.spring.annotation.MapperScan;
 @EnableScaffoldWebSecurityConfiguration // web security
 @Configuration
 public class CommonsConfiguration {
+
+  @Bean
+  public AnnotationUrlRegistryConfigurerCustomizer annotationUrlAuthorizationConfigurerCustomizer() {
+    return new AnnotationUrlRegistryConfigurerCustomizer() {
+      @Override
+      public void customize(ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry registry) {
+        System.err.println(getAllMetadata().stream()
+            .map(metadata -> String.format("[%s], [%s]",
+                StringUtils.join(metadata.getUris(), ", "),
+                StringUtils.join(metadata.getMethodTypes(), ", "))
+            )
+            .collect(Collectors.joining("\n")));
+        super.customize(registry);
+      }
+    };
+  }
+
 }
